624338d752e1a0b190f74ffd76b29ec1b1abf32a
octa96mz <octa96mz@gmail.com>
2022-07-19 21:31:54 -0300

    Formularios, React Context y completando el carrito de compras

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from '../containers/Layout';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import PasswordRecovery from '../pages/PasswordRecovery';
-import SendEmail from '../pages/SendEmail';
-import NewPassword from '../pages/NewPassword';
-import MyAccount from '../pages/MyAccount';
-import CreateAccount from '../pages/CreateAccount';
-import Checkout from '../pages/Checkout';
-import Orders from '../pages/Orders';
-import NotFound from '../pages/NotFound';
-import '../styles/global.css';
+import Layout from '@containers/Layout';
+import Home from '@pages/Home';
+import Login from '@pages/Login';
+import PasswordRecovery from '@pages/PasswordRecovery';
+import SendEmail from '@pages/SendEmail';
+import NewPassword from '@pages/NewPassword';
+import MyAccount from '@pages/MyAccount';
+import CreateAccount from '@pages/CreateAccount';
+import Checkout from '@pages/Checkout';
+import Orders from '@pages/Orders';
+import NotFound from '@pages/NotFound';
+import AppContext from "@context/AppContext";
+import '@styles/global.css';
+import useInitialState from "@hooks/useInitialState";
 
 const App = () => {
+    const InitialState = useInitialState(AppContext);
     return (
+        <AppContext.Provider value= {InitialState}>
         <BrowserRouter>
             <Layout>
                 <Routes>
@@ -31,6 +35,7 @@ const App = () => {
                 </Routes>
             </Layout>
         </BrowserRouter>
+        </AppContext.Provider>
     );
 }
 
